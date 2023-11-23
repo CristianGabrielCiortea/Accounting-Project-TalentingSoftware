3858f1c6bc1e76e1cbd97792cc65a43ce903b938
Cristian <cristian.ciortea@student.unitbv.ro>
2023-11-23 17:07:27 +0200

    Added Employer entity,DbContext and created the db

---
+++ AccountingApp/AccountingApp.Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AccountingApp.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AccountingApp.Server.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
